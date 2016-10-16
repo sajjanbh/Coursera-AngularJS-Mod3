720e826b6871dbfc86194b93442d455e52cff4fe
Sajjan Bhattarai <sajjanbhattarai@gmail.com>
2016-10-16 16:15:41 +0545

    Working Code!

---
+++ js/app.js
@@ -0,0 +1,89 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController )
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective)
+.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: '../foundItems.template.html',
+    scope: {
+      items: '<',
+      myTitle: '@title',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'narrow',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+function FoundItemsDirectiveController() {
+  var narrow = this;
+}
+
+NarrowItDownController .$inject = ['MenuSearchService', '$timeout'];
+function NarrowItDownController(MenuSearchService, $timeout) {
+  var narrow = this;
+  narrow.searchTerm = "";
+  narrow.found = MenuSearchService.getItems();
+
+  var origTitle = "Found Items";
+  //narrow.title = origTitle + " (" + narrow.found.length + " items )";
+
+  // Function triggered on clicking "Narrow It Down" button
+  // Pulls searchTerm from input field and fetches matching items containing searchTerm in their description
+  narrow.getMatchedMenuItems = function () {
+    MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
+    $timeout(function () {
+      console.log(narrow.found);
+      narrow.title = origTitle + " (" + narrow.found.length + " items )";
+    }, 1000);
+  }
+
+  // Function to remove unwanted item from the found array
+  narrow.removeItem = function (itemIndex) {
+    MenuSearchService.removeItem(itemIndex);
+    narrow.title = origTitle + " (" + narrow.found.length + " items )";
+  }
+}
+
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath']
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+  var matched_items = [];
+
+  service.getItems = function () {
+    return matched_items;
+  }
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    matched_items.length = 0;
+    $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    }).then(function (response) {
+      var all_items = response.data.menu_items;
+      for(var i = 0; i < all_items.length; i++) {
+        if (all_items[i].description.indexOf(searchTerm) !== -1) {
+          matched_items.push(all_items[i]);
+        }
+      }
+    })
+    .catch(function (error) {
+      console.log("Something went terribly wrong.");
+    });
+  }
+
+  service.removeItem = function (itemIndex) {
+    matched_items.splice(itemIndex, 1);
+  };
+}
+
+})();
